c921e5e5b36b235ba20dbf540f26f5d3fa474c7f
vvcb <vvcb.n1@gmail.com>
2023-09-11 12:29:32 +0100

    Add logging to RunAnalysis

--- Diagnostics/1a_fluroquinolones_diagnostics/RunAnalysis.R
+++ Diagnostics/1a_fluroquinolones_diagnostics/RunAnalysis.R
@@ -1,9 +1,13 @@
 
 # load drug concepts ----
 # note, these were identified in drug_concepts/concept_search.R
+library(logger)
+
+logger::log_info("Loading drug concepts...")
 drug_concepts <- read.csv(here("drug_concepts", "drug_concepts.csv"))
 
 # run diagnostics ----
+logger::log_info("Running drug exposure diagnostics...this could take a while.")
 drug_diagnostics <- suppressWarnings(suppressMessages(
   # uninformative warnings because we're not running all checks
   DrugExposureDiagnostics::executeChecks(
@@ -17,6 +21,7 @@ drug_diagnostics <- suppressWarnings(suppressMessages(
 ))
 
 # export diagnostics ----
+logger::log_info("Writing results to results folder...")
 writeResultToDisk(drug_diagnostics,
                   databaseId = database_name,
                   outputFolder = here("results"))
