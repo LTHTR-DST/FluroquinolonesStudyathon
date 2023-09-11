d4ce4816edff779ed3f980ded2d56fc0b51bfd49
vvcb <vvcb.n1@gmail.com>
2023-09-11 11:52:46 +0100

    Read parameters from .env file

--- Diagnostics/1a_fluroquinolones_diagnostics/CodeToRun.R
+++ Diagnostics/1a_fluroquinolones_diagnostics/CodeToRun.R
@@ -6,15 +6,24 @@ library(DBI)
 library(CDMConnector)
 library(DrugExposureDiagnostics)
 library(here)
+library(dotenv)
 
-# database name ----
-database_name <- "...."
+# Load environment variabales from root directory
+dotenv::load_dot_env(here("../../.env"))
+
+dsn <- Sys.getenv("ODBC_DSN")
+database_name <-  Sys.getenv("DATABASE_NAME")
+cdm_schema <- Sys.getenv("CDM_SCHEMA")
+cdm_version <- Sys.getenv("CDM_VERSION")
+cdm_name <- Sys.getenv("CDM_NAME")
+
 
 # database connection ----
 # see https://darwin-eu.github.io/CDMConnector/articles/a04_DBI_connection_examples.html
 # for examples on how to connect and create your cdm reference
-db <- dbConnect(.....)
 
-cdm <- cdmFromCon(.....)
+db <- dbConnect(odbc::odbc(), dsn, Database = database_name,  timeout = 10)
+
+cdm <- CDMConnector::cdm_from_con(db, cdm_schema=cdm_schema, cdm_name = cdm_name, cdm_version = cdm_version)
 
 source(here("RunAnalysis.R"))
